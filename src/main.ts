0f4bf6c0a43d32fca70a09edf1b78d9a441d7082
Duc Anh <nducanh1010@gmail.com>
2023-06-19 14:14:04 +0700

    add view ejs, config main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public')); // cho phép truy cập thư mục css, js, images
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));  // nơi store html
+  app.setViewEngine('ejs');
   await app.listen(3000);
 }
 bootstrap();
