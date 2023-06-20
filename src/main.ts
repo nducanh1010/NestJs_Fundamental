ff519287c4d46e10f70afd016dafbd61b29fe149
Duc Anh <nducanh1010@gmail.com>
2023-06-20 14:08:58 +0700

    validate data

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import {ConfigService} from '@nestjs/config'
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService=app.get(ConfigService)
   app.useStaticAssets(join(__dirname, '..', 'public')); // cho phép truy cập thư mục css, js, images
   app.setBaseViewsDir(join(__dirname, '..', 'views'));  // nơi store html
   app.setViewEngine('ejs');
+  app.useGlobalPipes( new ValidationPipe())
   await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
