7c419dee5e27b49297498636634119dbcc285ded
Duc Anh <nducanh1010@gmail.com>
2023-07-03 14:16:51 +0700

    addition  field in user schema

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,18 +1,78 @@
-import { IsEmail, IsNotEmpty } from "class-validator";
+import { Type } from 'class-transformer';
+import { IsEmail, IsNotEmpty, IsNotEmptyObject, IsObject, ValidateNested } from 'class-validator';
+import mongoose from 'mongoose';
 
 //data transfer object // class = object
-export class CreateUserDto {
-    @IsEmail({},{
-       message: 'Email không đúng định dạng'
-    })
-    @IsNotEmpty({
-        message:'Email không được để trống'
-    })
-    email:string;
-    @IsNotEmpty({
-        message:'Password không được để trống'
-    })
-    password:string;
+class Company{
+    @IsNotEmpty()
+    _id:mongoose.Schema.Types.ObjectId;
+    @IsNotEmpty()
     name:string;
-    address:string;
+}
+export class CreateUserDto {
+  @IsEmail(
+    {},
+    {
+      message: 'Email không đúng định dạng',
+    },
+  )
+  @IsNotEmpty({
+    message: 'Email không được để trống',
+  })
+  email: string;
+  @IsNotEmpty({
+    message: 'Password không được để trống',
+  })
+  password: string;
+  @IsNotEmpty({
+    message: 'Name không được để trống',
+  })
+  name: string;
+  @IsNotEmpty({
+    message: 'Age không được để trống',
+  })
+  age: number;
+  @IsNotEmpty({
+    message: 'Gender không được để trống',
+  })
+  gender: number;
+  address: string;
+  @IsNotEmpty({ message: 'Role không được để trống' })
+  role: string;
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(()=>Company)
+  company:Company
+}
+export class RegisterUserDto {
+  @IsEmail(
+    {},
+    {
+      message: 'Email không đúng định dạng',
+    },
+  )
+  @IsNotEmpty({
+    message: 'Email không được để trống',
+  })
+  email: string;
+  @IsNotEmpty({
+    message: 'Password không được để trống',
+  })
+  password: string;
+  @IsNotEmpty({
+    message: 'Name không được để trống',
+  })
+  name: string;
+  @IsNotEmpty({
+    message: 'Age không được để trống',
+  })
+  age: number;
+  @IsNotEmpty({
+    message: 'Gender không được để trống',
+  })
+  gender: number;
+  address: string;
+  @IsNotEmpty({ message: 'Role không được để trống' })
+  role: string;
 }
--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,23 +1,47 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>; // tham chiếu xuống mongo db tạo ra table
 
-@Schema({timestamps:true})
+@Schema({ timestamps: true })
 export class User {
+  @Prop()
+  name: string;
   @Prop({ required: true }) // validate
   email: string;
-
   @Prop({ required: true })
   password: string;
   @Prop()
-  phone: string;
-  @Prop()
-  name: string;
-  @Prop()
   age: number;
   @Prop()
-  address: string;
+  gender: string;
+
+  @Prop()
+  phone: string;
+  @Prop({ type: Object })
+  company: {
+    _id: mongoose.Schema.Types.ObjectId;
+    name: string;
+  };
+  @Prop({ type: Object })
+  createdBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+  @Prop()
+  role: string;
+  @Prop()
+  refreshToken: string;
+  @Prop({ type: Object })
+  updatedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+  @Prop({ type: Object })
+  deletedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
   @Prop()
   createdAt: Date;
   @Prop()
