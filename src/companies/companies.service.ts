0a7e45d6c47f6fa3a810310872b8bcfee00991b3
Duc Anh <nducanh1010@gmail.com>
2023-07-03 11:05:30 +0700

    add transform interceptor

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,91 +1,96 @@
-import {Injectable} from '@nestjs/common';
-import {CreateCompanyDto} from './dto/create-company.dto';
-import {UpdateCompanyDto} from './dto/update-company.dto';
-import {Company, CompanyDocument} from "./schemas/company.schema";
-import {SoftDeleteModel} from "soft-delete-plugin-mongoose";
-import {InjectModel} from "@nestjs/mongoose";
-import {IUser} from "../users/user.interface";
-import {User} from "../decorator/customize";
-import mongoose from "mongoose";
-import aqp from 'api-query-params';
-
+import { Injectable } from '@nestjs/common';
+import { CreateCompanyDto } from './dto/create-company.dto';
+import { UpdateCompanyDto } from './dto/update-company.dto';
+import { Company, CompanyDocument } from './schemas/company.schema';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { IUser } from '../users/user.interface';
+import { User } from '../decorator/customize';
+import mongoose from 'mongoose';
+import aqp from 'api-query-params'
 @Injectable()
 export class CompaniesService {
-    constructor(
-        @InjectModel(Company.name)
-        private CompanyModel: SoftDeleteModel<CompanyDocument>
-    ) {
-    }
+  constructor(
+    @InjectModel(Company.name)
+    private CompanyModel: SoftDeleteModel<CompanyDocument>,
+  ) {}
 
-    async create(createCompanyDto: CreateCompanyDto, user: IUser) {
-        // const newCompany = await this.CompanyModel.create(
-        //     {
-        //       name:createCompanyDto.name,
-        //       address:createCompanyDto.address,
-        //       description:createCompanyDto.description
-        //     }
-        // )
+  async create(createCompanyDto: CreateCompanyDto, user: IUser) {
+    // const newCompany = await this.CompanyModel.create(
+    //     {
+    //       name:createCompanyDto.name,
+    //       address:createCompanyDto.address,
+    //       description:createCompanyDto.description
+    //     }
+    // )
 
-        // return newCompany
-        return this.CompanyModel.create({
-            ...createCompanyDto,
-            createdBy: {
-                _id: user._id,
-                email: user.email,
-            }
-        })
-    }
+    // return newCompany
+    return this.CompanyModel.create({
+      ...createCompanyDto,
+      createdBy: {
+        _id: user._id,
+        email: user.email,
+      },
+    });
+  }
 
-    async findAll(currentPage:number,limit:number,qs:string) {
-        const { filter, skip, sort, projection, population } = aqp(qs);
-        delete  filter.page
-        delete  filter.limit
-        let offset = (+currentPage - 1) * (+limit); // bỏ qua bao nhiêu bản ghi
-        let defaultLimit = +limit ? +limit : 10;
-        const totalItems = (await this.CompanyModel.find(filter)).length;
-        const totalPages = Math.ceil(totalItems / defaultLimit); // tổng số trang
-        const result = await this.CompanyModel.find(filter)
-            .skip(offset)
-            .limit(defaultLimit)
-            // @ts-ignore: Unreachable code error
-            .sort(sort)
-            .populate(population)
-            .exec()
-        return {
-            meta: {
-                current: currentPage, //trang hiện tại
-                pageSize: limit, //số lượng bản ghi đã lấy
-                pages: totalPages, //tổng số trang với điều kiện query
-                total: totalItems // tổng số phần tử (số bản ghi)
-            },
-            result //kết quả query
-        }
+  async findAll(currentPage: number, limit: number, qs: string) {
+    const { filter, skip, sort, projection, population } = aqp(qs);
+    delete filter.page;
+    delete filter.limit;
+    let offset = (+currentPage - 1) * +limit; // bỏ qua bao nhiêu bản ghi
+    let defaultLimit = +limit ? +limit : 10;
+    const totalItems = (await this.CompanyModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit); // tổng số trang
+    const result = await this.CompanyModel.find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      // @ts-ignore: Unreachable code error
+      .sort(sort as any)
+      .populate(population) // join bảng
+      
+      .exec();
+    return {
+      meta: {
+        current: currentPage, //trang hiện tại
+        pageSize: limit, //số lượng bản ghi đã lấy
+        pages: totalPages, //tổng số trang với điều kiện query
+        total: totalItems, // tổng số phần tử (số bản ghi)
+      },
+      result, //kết quả query
+    };
+  }
 
-    }
+  findOne(id: number) {
+    return `This action returns a #${id} company`;
+  }
 
-    findOne(id: number) {
-        return `This action returns a #${id} company`;
-    }
+  async update(id: string, updateCompanyDto: UpdateCompanyDto, user: IUser) {
+    return this.CompanyModel.updateOne(
+      { _id: id },
+      {
+        ...updateCompanyDto,
+        updatedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
+  }
 
-    async update(id: string, updateCompanyDto: UpdateCompanyDto, user: IUser) {
-        return this.CompanyModel.updateOne(
-            {_id: id},
-            {
-                ...updateCompanyDto,
-                updatedBy: {
-                    _id: user._id,
-                    email: user.email,
-                }
-            },
-        );
-    }
-
-    async remove(id: string, user:IUser) {
-        if (!mongoose.Types.ObjectId.isValid(id)) return 'not found company';
-        await  this.CompanyModel.updateOne({_id: id},{deletedBy:{
-                _id:user._id,
-                email:user.email
-            }})
-        return this.CompanyModel.softDelete({_id: id});
-    }
+  async remove(id: string, user: IUser) {
+    if (!mongoose.Types.ObjectId.isValid(id)) return 'not found company';
+    await this.CompanyModel.updateOne(
+      { _id: id },
+      {
+        deletedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
+    return this.CompanyModel.softDelete({ _id: id });
+  }
 }
+
+
--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,27 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import {ConfigService} from '@nestjs/config'
+import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { TransformInterceptor } from './core/transform.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const configService=app.get(ConfigService)
-  const reflector=app.get(Reflector)
-  app.useGlobalGuards(new JwtAuthGuard(reflector))
+  const configService = app.get(ConfigService);
+  const reflector = app.get(Reflector);
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
   app.useStaticAssets(join(__dirname, '..', 'public')); // cho phép truy cập thư mục css, js, images
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));  // nơi store html
+  app.setBaseViewsDir(join(__dirname, '..', 'views')); // nơi store html
   app.setViewEngine('ejs');
-  app.useGlobalPipes( new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
   // config CORS
-  app.enableCors(
-    {
-      "origin": "*", // cho phép nơi nào có thể kết nối tới
-      "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-      "preflightContinue": false,
-      "optionsSuccessStatus": 204
-      }
-  );
+  app.enableCors({
+    origin: '*', // cho phép nơi nào có thể kết nối tới
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
