16d24a36d90a4afba9c88c6cc7ddc3bb72cd5533
Duc Anh <nducanh1010@gmail.com>
2023-07-20 17:26:36 +0700

    validate filetype upload

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,45 +1,71 @@
-import {Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile} from '@nestjs/common';
-import {FilesService} from './files.service';
-import {CreateFileDto} from './dto/create-file.dto';
-import {UpdateFileDto} from './dto/update-file.dto';
-import {FileInterceptor} from "@nestjs/platform-express";
-import {Public} from "../decorator/customize";
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFile,
+  HttpStatus,
+  ParseFilePipeBuilder,
+} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { CreateFileDto } from './dto/create-file.dto';
+import { UpdateFileDto } from './dto/update-file.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Public } from '../decorator/customize';
 
 @Controller('files')
 export class FilesController {
-    constructor(private readonly filesService: FilesService) {
-    }
+  constructor(private readonly filesService: FilesService) {}
 
-    @Public()
-    @Post('upload')
-    // middleware can thiệp vào file của NestJs, nhặt ra được fieldName, tên file, loại file, cỡ file, ..
-    @UseInterceptors(FileInterceptor('file'))  // truyền form data, key : file
-    uploadFile(@UploadedFile() file: Express.Multer.File) {
-        console.log(file);
-    }
+  @Public()
+  @Post('upload')
+  // middleware can thiệp vào file của NestJs, nhặt ra được fieldName, tên file, loại file, cỡ file, ..
+  @UseInterceptors(FileInterceptor('file')) // truyền form data, key : file
+  uploadFile(
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: /^(image\/(jpeg|png|gif)|text\/(plain|html)|application\/(pdf|msword|vnd.openxmlformats-officedocument.wordprocessingml.document))$/i
+          ,    //validate loại file
+        })
+        .addMaxSizeValidator({
+          maxSize: 1024*1024, //1MB
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY, // mã lỗi 422 không thể xử lí
+        }),
+    )
+    file: Express.Multer.File,
+  ) {
+    console.log(file);
+  }
 
-    @Post()
-    create(@Body() createFileDto: CreateFileDto) {
-        return this.filesService.create(createFileDto);
-    }
+  @Post()
+  create(@Body() createFileDto: CreateFileDto) {
+    return this.filesService.create(createFileDto);
+  }
 
-    @Get()
-    findAll() {
-        return this.filesService.findAll();
-    }
+  @Get()
+  findAll() {
+    return this.filesService.findAll();
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.filesService.findOne(+id);
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.filesService.findOne(+id);
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updateFileDto: UpdateFileDto) {
-        return this.filesService.update(+id, updateFileDto);
-    }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateFileDto: UpdateFileDto) {
+    return this.filesService.update(+id, updateFileDto);
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.filesService.remove(+id);
-    }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.filesService.remove(+id);
+  }
 }
