0b018e627f466a756d5e269bf2244b7e3659c722
Duc Anh <nducanh1010@gmail.com>
2023-07-27 17:09:55 +0700

    init fake data through database

---
+++ src/databases/databases.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectModel } from '@nestjs/mongoose';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import {
+  Permission,
+  PermissionDocument,
+} from 'src/permissions/schemas/permission.schema';
+import { Role, RoleDocument } from 'src/roles/schemas/role.schema';
+import { User, UserDocument } from 'src/users/schemas/user.schema';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class DatabasesService implements OnModuleInit {
+  constructor(
+    @InjectModel(Role.name)
+    private roleModel: SoftDeleteModel<RoleDocument>,
+    @InjectModel(Permission.name)
+    private permissionModel: SoftDeleteModel<PermissionDocument>,
+    @InjectModel(User.name)
+    private userModel: SoftDeleteModel<UserDocument>,
+    private configService: ConfigService,
+    private userService: UsersService,
+  ) {}
+  onModuleInit() {
+    console.log('THe module initialize');
+  }
+}
