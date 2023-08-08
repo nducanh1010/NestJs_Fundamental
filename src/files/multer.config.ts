67fa711300c5bcd9cceea3be7bd7916327575498
duc anh <nducanh1010@gmail.com>
2023-08-08 23:14:01 +0700

    modify file controller, add message exception file controller

--- src/files/multer.config.ts
+++ src/files/multer.config.ts
@@ -1,62 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
 import {
-  MulterModuleOptions,
-  MulterOptionsFactory,
+    MulterModuleOptions,
+    MulterOptionsFactory,
 } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
-import path, { join } from 'path';
+import {diskStorage} from 'multer';
+import path, {join} from 'path';
 import fs from 'fs';
+
 @Injectable()
 export class MulterConfigService implements MulterOptionsFactory {
-  // trả về đường link thư mục root
-  getRootPath = () => {
-    return process.cwd();
-  };
-  // check nếu thư mục không tồn tại, tạo thư mục đấy
-  ensureExists(targetDirectory: string) {
-    //fs là thư viện filestream
-    fs.mkdir(targetDirectory, { recursive: true }, (error) => {
-      if (!error) {
-        console.log('Directory successfully created, or it already exists.');
-        return;
-      }
-      switch (error.code) {
-        case 'EEXIST':
-          // Error:
-          // Requested location already exists, but it's not a directory.
-          break;
-        case 'ENOTDIR':
-          // Error:
-          // The parent hierarchy contains a file with the same name as the dir
-          // you're trying to create.
-          break;
-        default:
-          // Some other error like permission denied.
-          console.error(error);
-          break;
-      }
-    });
-  }
-  createMulterOptions(): MulterModuleOptions {
-    return {
-      // cấu hình thằng multer xem lưu dữ liệu ở đâu
-      storage: diskStorage({
-        // ghi đè destnation, lấy động file dựa trên request header
-        destination: (req, file, cb) => {
-          const folder = req?.headers?.folder_type ?? 'default';
-          this.ensureExists(`public/images/${folder}`);
-          cb(null, join(this.getRootPath(), `public/images/${folder}`));
-        },
-        // đổi được tên file  
-        filename: (req, file, cb) => {
-          //get image extension
-          let extName = path.extname(file.originalname);
-          //get image's name (without extension)
-          let baseName = path.basename(file.originalname, extName);
-          let finalName = `${baseName}-${Date.now()}${extName}`;
-          cb(null, finalName);
-        },
-      }),
+    // trả về đường link thư mục root
+    getRootPath = () => {
+        return process.cwd();
     };
-  }
+
+    // check nếu thư mục không tồn tại, tạo thư mục đấy
+    ensureExists(targetDirectory: string) {
+        //fs là thư viện filestream
+        fs.mkdir(targetDirectory, {recursive: true}, (error) => {
+            if (!error) {
+                console.log('Directory successfully created, or it already exists.');
+                return;
+            }
+            switch (error.code) {
+                case 'EEXIST':
+                    // Error:
+                    // Requested location already exists, but it's not a directory.
+                    break;
+                case 'ENOTDIR':
+                    // Error:
+                    // The parent hierarchy contains a file with the same name as the dir
+                    // you're trying to create.
+                    break;
+                default:
+                    // Some other error like permission denied.
+                    console.error(error);
+                    break;
+            }
+        });
+    }
+
+    createMulterOptions(): MulterModuleOptions {
+        return {
+            // cấu hình thằng multer xem lưu dữ liệu ở đâu
+            storage: diskStorage({
+                // ghi đè destnation, lấy động file dựa trên request header
+                destination: (req, file, cb) => {
+                    const folder = req?.headers?.folder_type ?? 'default';
+                    this.ensureExists(`public/images/${folder}`);
+                    cb(null, join(this.getRootPath(), `public/images/${folder}`));
+                },
+                // đổi được tên file
+                filename: (req, file, cb) => {
+                    //get image extension
+                    let extName = path.extname(file.originalname);
+                    //get image's name (without extension)
+                    let baseName = path.basename(file.originalname, extName);
+                    let finalName = `${baseName}-${Date.now()}${extName}`;
+                    cb(null, finalName);
+                },
+            }),
+//cb = callback
+            fileFilter: (req, file, cb) => {
+                const allowedFileTypes = ['jpg', 'jpeg', 'png', 'gif', 'pdf', 'doc', 'docx'];
+                const fileExtension = file.originalname.split('.').pop().toLowerCase();
+                const isValidFileType = allowedFileTypes.includes(fileExtension);
+                if (!isValidFileType) {
+                    cb(new HttpException('Invalid file type', HttpStatus.UNPROCESSABLE_ENTITY), null);
+                } else
+                    cb(null, true);
+            },
+            limits: {
+                fileSize: 1024 * 1024 * 1 // 1MB
+            }
+        };
+    }
 }
