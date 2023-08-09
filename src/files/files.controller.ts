4caac181a88a948789df58925d4b9204342a6c8b
Duc Anh <nducanh1010@gmail.com>
2023-08-09 11:49:36 +0700

    update fix create resume

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,63 +1,63 @@
 import {
-    Controller,
-    Get,
-    Post,
-    Body,
-    Patch,
-    Param,
-    Delete,
-    UseInterceptors,
-    UploadedFile,
-    HttpStatus,
-    ParseFilePipeBuilder, UseFilters,
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFile,
+  HttpStatus,
+  ParseFilePipeBuilder,
+  UseFilters,
 } from '@nestjs/common';
-import {FilesService} from './files.service';
-import {CreateFileDto} from './dto/create-file.dto';
-import {UpdateFileDto} from './dto/update-file.dto';
-import {FileInterceptor} from '@nestjs/platform-express';
-import {Public, ResponseMessage} from '../decorator/customize';
-import {ApiTags} from '@nestjs/swagger';
-import {HttpExceptionFilter} from "../core/http-exception.filter";
+import { FilesService } from './files.service';
+import { CreateFileDto } from './dto/create-file.dto';
+import { UpdateFileDto } from './dto/update-file.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Public, ResponseMessage } from '../decorator/customize';
+import { ApiTags } from '@nestjs/swagger';
+import { HttpExceptionFilter } from '../core/http-exception.filter';
 
 @ApiTags('File')
 @Controller('files')
 export class FilesController {
-    constructor(private readonly filesService: FilesService) {
-    }
+  constructor(private readonly filesService: FilesService) {}
 
-    @Public()
-    @Post('upload')
-    @ResponseMessage('Upload single File')
-    // middleware can thiệp vào file của NestJs, nhặt ra được fieldName, tên file, loại file, cỡ file, ..
-    @UseInterceptors(FileInterceptor('fileUpload')) // truyền form data, key : fileUpload
-    @UseFilters(new HttpExceptionFilter())
-uploadFile(@UploadedFile()file:Express.Multer.File){
-        return{
-            filename:file.filename
-        }
-    }
-    @Post()
-    create(@Body() createFileDto: CreateFileDto) {
-        return this.filesService.create(createFileDto);
-    }
+  @Public()
+  @Post('upload')
+  @ResponseMessage('Upload single File')
+  // middleware can thiệp vào file của NestJs, nhặt ra được fieldName, tên file, loại file, cỡ file, ..
+  @UseInterceptors(FileInterceptor('fileUpload')) // truyền form data, key : fileUpload
+  @UseFilters(new HttpExceptionFilter())
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return {
+      fileName: file.filename,
+    };
+  }
+  @Post()
+  create(@Body() createFileDto: CreateFileDto) {
+    return this.filesService.create(createFileDto);
+  }
 
-    @Get()
-    findAll() {
-        return this.filesService.findAll();
-    }
+  @Get()
+  findAll() {
+    return this.filesService.findAll();
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.filesService.findOne(+id);
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.filesService.findOne(+id);
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updateFileDto: UpdateFileDto) {
-        return this.filesService.update(+id, updateFileDto);
-    }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateFileDto: UpdateFileDto) {
+    return this.filesService.update(+id, updateFileDto);
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.filesService.remove(+id);
-    }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.filesService.remove(+id);
+  }
 }
